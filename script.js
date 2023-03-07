c28489e5b317c6ee3e3d04d8e9c7e407e945e895
Alex <electromanati@gmail.com>
2023-03-07 12:14:45 +0100

    hogwarts project initialized

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {}
+
+function loadJSON() {}
+
+function prepareData() {}
+
+function displayData() {}
+
+function hackTheSystem() {}
